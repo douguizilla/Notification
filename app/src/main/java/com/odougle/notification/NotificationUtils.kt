94fc55377accb2d28bf1872da79f31d7afdb7b00
douguizilla <douglasgommez@gmail.com>
2021-09-07 14:31:32 -0300

    implemented createNotificationChannel

--- app/src/main/java/com/odougle/notification/NotificationUtils.kt
+++ app/src/main/java/com/odougle/notification/NotificationUtils.kt
@@ -1,4 +1,29 @@
 package com.odougle.notification
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.Color
+import android.os.Build
+import androidx.annotation.RequiresApi
+
 object NotificationUtils {
+    val CHANNEL_ID = "default"
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(context: Context){
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val channelName = context.getString(R.string.notif_channel_name)
+        val channelDescription = context.getString(R.string.notif_channel_description)
+        val channel = NotificationChannel(CHANNEL_ID,
+        channelName,
+        NotificationManager.IMPORTANCE_DEFAULT).apply {
+            description = channelDescription
+            enableLights(true)
+            lightColor = Color.GREEN
+            enableVibration(true)
+            vibrationPattern = longArrayOf(100,200,300,400,500,400,300,200,100)
+        }
+        notificationManager.createNotificationChannel(channel)
+    }
 }
