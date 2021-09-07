96170b23149d5ad8fef24bab92f7f96763aed78b
douguizilla <douglasgommez@gmail.com>
2021-09-07 14:20:35 -0300

    set object NotificationUtils call inside button click action

--- app/src/main/java/com/odougle/notification/MainActivity.kt
+++ app/src/main/java/com/odougle/notification/MainActivity.kt
@@ -15,31 +15,31 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnSimple.setOnClickListener {
-
+            NotificationUtils.notificationSimple(this)
         }
 
         binding.btnTapAction.setOnClickListener {
-
+            NotificationUtils.notificationWithTapAction(this)
         }
 
         binding.btnBigText.setOnClickListener {
-
+            NotificationUtils.notificationBigText(this)
         }
 
         binding.btnActionButton.setOnClickListener {
-
+            NotificationUtils.notificationWithButtonAction(this)
         }
 
         binding.btnDirectReply.setOnClickListener {
-
+            NotificationUtils.notificationAutoReply(this)
         }
 
         binding.btnInbox.setOnClickListener {
-
+            NotificationUtils.notificationInbox(this)
         }
 
         binding.btnHeadsUp.setOnClickListener {
-
+            NotificationUtils.notificationHeadsUp(this)
         }
 
 
