f80579a1c47a8422a79ffb9591813c906bd6dfe9
douguizilla <douglasgommez@gmail.com>
2021-09-07 15:06:54 -0300

    set view binding on DetailsActivity.kt

--- app/src/main/java/com/odougle/notification/DetailsActivity.kt
+++ app/src/main/java/com/odougle/notification/DetailsActivity.kt
@@ -2,10 +2,17 @@ package com.odougle.notification
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.odougle.notification.databinding.ActivityDetailsBinding
+import com.odougle.notification.databinding.ActivityMainBinding
 
 class DetailsActivity : AppCompatActivity() {
+
+    private val binding : ActivityDetailsBinding by lazy {
+        ActivityDetailsBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_details)
+        setContentView(binding.root)
     }
 }
