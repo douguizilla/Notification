d6b3bd3c5b3e08c483ee044e01fd3c09ccf040a5
douguizilla <douglasgommez@gmail.com>
2021-09-07 14:58:18 -0300

    implemented getContentIntent

--- app/src/main/java/com/odougle/notification/NotificationUtils.kt
+++ app/src/main/java/com/odougle/notification/NotificationUtils.kt
@@ -3,7 +3,9 @@ package com.odougle.notification
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.os.Build
 import androidx.annotation.RequiresApi
@@ -63,6 +65,13 @@ object NotificationUtils {
     }
 
     fun notificationHeadsUp(context: Context) {
+    }
+
+    private fun getContentIntent(context: Context): PendingIntent? {
+        val intent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        return PendingIntent.getActivity(context, 0, intent, 0)
     }
 
 
