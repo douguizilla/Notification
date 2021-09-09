0495abbee65745dac1e9ee562850eb7a9879a206
douguizilla <douglasgommez@gmail.com>
2021-09-09 16:58:37 -0300

    implemented ReplyReceiver

---
+++ app/src/main/java/com/odougle/notification/receivers/ReplyReceiver.kt
@@ -0,0 +1,26 @@
+package com.odougle.notification.receivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+import androidx.core.app.RemoteInput
+import com.odougle.notification.NotificationUtils
+
+class ReplyReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val remoteInput = RemoteInput.getResultsFromIntent(intent)
+        val message = remoteInput?.getCharSequence(EXTRA_TEXT_REPLY)
+        if(message != null){
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+        }
+        val notificationId = intent.getIntExtra(EXTRA_NOTIFICATION_ID, -1)
+        NotificationUtils.notificationReplied(context, notificationId)
+    }
+
+    companion object{
+        const val EXTRA_TEXT_REPLY = "action_text_reply"
+        const val EXTRA_NOTIFICATION_ID = "notif_id"
+    }
+
+}
